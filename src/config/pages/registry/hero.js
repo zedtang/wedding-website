745a9bb1dd033d4d40445ef5d347d5ef9e495622
Jiaheng Tang <jiaheng1@vt.edu>
2024-06-17 22:32:27 -0700

    style registry page

---
+++ src/config/pages/registry/hero.js
@@ -0,0 +1,21 @@
+// Hero configuration on the gallery page
+
+const hero = {
+  // If the entire hero section should be enabled or not
+  enabled: true,
+  padding: 'pt-12 md:pt-20',
+
+  // Hero title with HTML support
+  title: {
+    enabled: true, // If the title should be enabled or not
+    text: `<span class="fancy">Wedding Registry</span>`,
+  },
+
+  // Hero subtitle with HTML support
+  subtitle: {
+    enabled: true, // If the subtitle should be enabled or not
+    text: `<span class="fancy">Visit our wedding registry.</span>`,
+  },
+};
+
+export default hero;
